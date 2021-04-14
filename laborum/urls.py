c2cab593432d70312a788dd485a0d3bb9fbbeb47
JAlejandroMG <jalejandromg@tutanota.com>
2021-04-14 11:15:16 -0500

    Added: Branch Tarea2

--- laborum/urls.py
+++ laborum/urls.py
@@ -8,6 +8,4 @@ urlpatterns = [
     path('empleados/', include('empleados.urls')),
     path('puestos/', include('puestos.urls')),
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
